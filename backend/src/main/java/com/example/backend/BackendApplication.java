b912d7b72200d6bcc8e83ccd69abafa0c53c16fa
Dominic <domlee2012@gmail.com>
2021-05-10 11:25:02 +0800

    Hello World

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -27,4 +29,10 @@ public class BackendApplication {
 		registrationBean.setOrder(0);
 		return registrationBean;
 	}
+
+	@GetMapping("/")
+	String hello() {
+		return "hello World";
+	}
+
 }
