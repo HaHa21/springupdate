6947e4df3ae0a6d9dfccb6371f29ca5b3bf666a5
Dominic <domlee2012@gmail.com>
2021-05-10 20:30:09 +0800

    hello change

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,22 +3,21 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class BackendApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-}
-
-@RestController
-class HelloController {
-	@GetMapping("/")
-	String hello() {
-		return "hello World";
-	}
 }
